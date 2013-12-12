cafdfe6234d30e487f1b4e0c1e3c4a2be1bd0bfa
luizhrqas <luizhrqas@gmail.com>
2013-12-12 01:19:12 -0300

    assets for sprockets updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery/jquery
 //= require jquery_ujs
+//= require bootstrap/dist/js/bootstrap
 //= require turbolinks
 //= require_tree .
