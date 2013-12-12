919ff9ee5e8b4add2cf4868c901425e32a219356
Luiz Henrique <luizhenrique@ubuntu.ubuntu-domain>
2013-12-11 23:06:48 -0300

    rails 4 brand new app generated without active record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forkbit::Application.config.secret_key_base = '37f5e4b202b6be200b1393f231498138a10b00f866af176e37a7d0b49ff1c2e006c98cadef51cbd418aaa69def20fe9e431846360b648e845959ae3916f3497c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Forkbit::Application.config.session_store :cookie_store, key: '_forkbit_session'
